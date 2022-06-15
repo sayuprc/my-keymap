a4ca6fe43a9daaa1e763357ff0c9995cd1a5a050
sayuprc <mkpoto15@gmail.com>
2022-06-15 21:13:56 +0900

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# my keymap
+
+自作キーボードのキーマップ
+
+[QMK Firmware](https://github.com/qmk/qmk_firmware)から必要なものをダウンロードして、`keyboards/利用するキーボード名/keymaps` にそれぞれ入れればよし
---
+++ claw44/keymaps/mymap/config.h
@@ -0,0 +1,46 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+//#define USE_MATRIX_I2C
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#define SSD1306OLED
+
+#define USE_SERIAL_PD2
+
+#define TAPPING_TERM 200
+
+#define RGB_DI_PIN D3
+
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 12
+#define RGBLIGHT_SPLIT
+#define RGBLED_SPLIT { 6, 6 }
+
+#define RGBLIGHT_LED_MAP { 0, 1, 2, 3, 4, 5, \
+													 11, 10, 9, 8, 7, 6 }
+
